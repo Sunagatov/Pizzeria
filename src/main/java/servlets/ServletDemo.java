177b3ee1d33a4d1ce373051303b6f4dd3fe6d3c5
Zufar Sunagatov <zuf999@mail.ru>
2017-07-12 01:15:24 +0400

    ????????? ???????.

---
+++ src/main/java/servlets/ServletDemo.java
@@ -0,0 +1,28 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Zufar on 12-Jul-17.
+ */
+@WebServlet("/ServletDemo")
+public class ServletDemo extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("Hello world!");
+        out.println("</body></html>");
+
+    }
+}
